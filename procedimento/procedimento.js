f68f446fc43ccc9ecc901d7e32fa17ec57772848
Aluno Técnico ADS1 <piresmarques2010@gmail.com>
2026-04-15 09:37:47 -0300

    procedimento

---
+++ procedimento/procedimento.js
@@ -0,0 +1,19 @@
+function procedimento(){
+
+    function inicio(){
+      
+    }
+    function mensagem(){
+        let linha = "-"
+        let i = 0;
+        let
+    
+        do{
+        linha = linha + "-"
+        i++;
+    }while( i < 50 );
+
+
+
+}
+}
