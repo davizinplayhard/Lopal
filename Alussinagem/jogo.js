06a081867d6a32cd93fb5ce45aca2daa8c245fe8
Aluno Técnico ADS1 <piresmarques2010@gmail.com>
2026-04-15 16:49:03 -0300

    primeira versão do jogo de posar na lua

--- Alussinagem/jogo.js
+++ Alussinagem/jogo.js
@@ -1,45 +1,146 @@
-/** @type {HTMLCanvasElement} */
 let canvas = document.querySelector("#jogo");
-let ctx = canvas.getContext("2d");
- 
-let moduloLunar = {
-    posicao: {
-        x: 100,
+let contexto = canvas.getContext("2d");
+let modulolunar = {
+    posicao:{
+        x: 700,
         y: 100
     },
+    ângulo: Math.PI/2,
     largura: 20,
     altura: 20,
-    cor: "#A0A3A1",
-    velocidade: {
-        x: 0,
-        y: 1.5
+    cor: "lightgray",
+    velocidade:{
+        x: -1.5,
+        y: 0
+    },
+    motorLigado: false,
+    combustível: 1000,
+    rotaçãoHorario: false,
+    rotaçãoAntihorario: false
+}
+function mostrarCombustível(){
+    contexto.font = "bold 18px Arial";
+    contexto.textAlign = "left";
+    contexto.textBaseline = "middle";
+    contexto.fillStyle = "White";
+    contexto.fillText(
+        `Combustível: ${(modulolunar.combustível * 0.1).toFixed(0)} %`,
+        50,
+        80
+    );
+}
+function mostrarVelocidadeVertical(){
+    contexto.font = "bold 18px Arial";
+    contexto.textAlign = "left";
+    contexto.textBaseline = "middle";
+    contexto.fillStyle = "White";
+    contexto.fillText(
+        `Velocidade: ${(modulolunar.velocidade.y * 10).toFixed(2)}`,
+        50,
+        60
+    );
+}
+function desenharFundo(){
+    contexto.clearRect(0, 0, canvas.width, canvas.height);
+    contexto.save();
+    contexto.fillStyle = "#000123"
+    contexto.fillRect(0, 0, canvas.width, canvas.height);
+    contexto.restore();
+}
+function desenharModuloLunar(){
+    contexto.save();
+    contexto.beginPath();
+    contexto.translate(modulolunar.posicao.x, modulolunar.posicao.y)
+    contexto.rotate(modulolunar.ângulo);
+    contexto.rect(modulolunar.largura * -0.5, modulolunar.altura * -0.5,
+        modulolunar.largura, modulolunar.altura);
+    contexto.fillStyle = modulolunar.cor;
+    contexto.fill();
+    contexto.closePath();
+   
+    if(modulolunar.motorLigado){
+        desenharChama();
+        modulolunar.combustível--;
+        if(modulolunar.combustível <= 0){
+            modulolunar.motorLigado = false;
+        }  
+    }
+   
+    contexto.restore();
+}
+function desenharChama(){
+    contexto.beginPath();
+    contexto.moveTo(modulolunar.largura * -0.5, modulolunar.altura * 0.5);
+    contexto.lineTo(modulolunar.largura * 0.5, modulolunar.altura * 0.5);
+    contexto.lineTo(0, modulolunar.altura * 0.5 + Math.random() * 35);
+    contexto.closePath();
+    contexto.fillStyle = "orange";
+    contexto.fill();
+}
+function desenhar(){
+    atraçãoGravitacional();
+    desenharFundo();
+    desenharModuloLunar();
+    mostrarCombustível();
+    mostrarVelocidadeVertical();
+ 
+    if(modulolunar.posicao.y > canvas.height -modulolunar.altura * 0.5){
+       if(modulolunar.velocidade.y <= 0.5){
+            mostrarResultado("Você Aterrissou na Lua com Sucesso!", cor = "green");
+        } else{
+            mostrarResultado("Você Espatifou a Nave na Lua!", cor = "red");
+        }
+        return
+    }
+    requestAnimationFrame(desenhar);
+}
+function mostrarResultado(mensagem, cor){
+    contexto.font = "bold 30px Arial";
+    contexto.textAlign = "center";
+    contexto.textBaseline = "middle";
+    contexto.fillStyle = cor;
+    contexto.fillText(mensagem, canvas.width * 0.5, canvas.height * 0.5);
+    cancelAnimationFrame(desenharModuloLunar, mostrarCombustível, mostrarVelocidadeVertical)
+ 
+}
+document.addEventListener("keydown", teclaPressionada);
+
+function teclaPressionada(evento) {
+    if (evento.key == "w" && modulolunar.combustível > 1) {
+        modulolunar.motorLigado = true;
+    } else if (evento.key == "d") {
+        modulolunar.rotaçãoHorario = true;
+    } else if (evento.key == "a") {
+        modulolunar.rotaçãoAntihorario = true;
     }
 }
+
+document.addEventListener("keyup", teclaSolta);
+
+function teclaSolta(evento) {
+    if (evento.key == "w") {
+        modulolunar.motorLigado = false;
+    } else if (evento.key == "d") {
+        modulolunar.rotaçãoHorario = false;
+    } else if (evento.key == "a") {
+        modulolunar.rotaçãoAntihorario = false;
+    }
+}
+const gravidade = 0.01;
+function atraçãoGravitacional(){
+    modulolunar.posicao.x += modulolunar.velocidade.x;
+    modulolunar.posicao.y += modulolunar.velocidade.y;
+    modulolunar.velocidade.y += gravidade;
  
-function desenhar() {
-    //atração gravitacional
-    moduloLunar.posicao.x += moduloLunar.velocidade.x;
-    moduloLunar.posicao.y += moduloLunar.velocidade.y;
- 
- 
-    //desenhar fundo da tela
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.save();
-    ctx.fillStyle = "#0a0a32";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.restore();
- 
-    //desenhar módulo lunar
-    ctx.save();
-    ctx.beginPath();
-    ctx.translate(moduloLunar.posicao.x, moduloLunar.posicao.y);
-    ctx.rect(moduloLunar.largura * -0.5, moduloLunar.altura * -0.5, moduloLunar.largura, moduloLunar.altura);
-    ctx.fillStyle = moduloLunar.cor;
-    ctx.fill();
-    ctx.restore();
- 
-    requestAnimationFrame(desenhar);
- 
+    if(modulolunar.rotaçãoHorario){
+        modulolunar.ângulo += Math.PI/180;
+    } else if(modulolunar.rotaçãoAntihorario){
+        modulolunar.ângulo -= Math.PI/180;
+    }
+    if(modulolunar.motorLigado){
+        modulolunar.velocidade.y -= 0.02 * Math.cos(modulolunar.ângulo)
+        modulolunar.velocidade.x += 0.02 * Math.sin(modulolunar.ângulo)
+    }
 }
 desenhar();
  
